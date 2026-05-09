5945f75ba7ebe80fd37cb67c4b933af978908ab3
Christian Areinamo <careinamoandrade@gmail.com>
2026-05-09 15:09:23 -0500

    initial commit

---
+++ src/functions/hello/handler.ts
@@ -0,0 +1,11 @@
+import type { APIGatewayProxyResultV2, APIGatewayProxyEventV2 } from 'aws-lambda';
+
+export const main = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hello from Canaima Backend!',
+      event,
+    }),
+  };
+};
